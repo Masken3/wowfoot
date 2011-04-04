4ef0a34baa4b7f40d4f877432111a6edfb1fac01
Masken3 <masken3@gmail.com>
2011-04-04 09:11:49 +0200

    Extract data from WorldMapContinent.dbc.

--- wowfoot-ex/src/wowfoot-ex.cpp
+++ wowfoot-ex/src/wowfoot-ex.cpp
@@ -1,10 +1,30 @@
 #include "config.h"
 #include "libs/mpq_libmpq04.h"
+#include "libs/dbcfile.h"
 
 int main() {
+	printf("Opening locale.mpq...\n");
 	MPQArchive locale(WOW_INSTALL_DIR"Data/"WOW_LOCALE"/locale-"WOW_LOCALE".MPQ");
-	MPQFile wmc("DBFilesClient\\WorldMapContinent.dbc");
-	printf("size: %i\n", wmc.getSize());
+
+	printf("Opening WorldMapContinent.dbc...\n");
+	DBCFile wmc("DBFilesClient\\WorldMapContinent.dbc");
+	bool res = wmc.open();
+	if(!res)
+		return 1;
+	printf("Extracting %i continents...\n", wmc.getRecordCount());
+	for(DBCFile::Iterator itr = wmc.begin(); itr != wmc.end(); ++itr) {
+		const DBCFile::Record& r(*itr);
+		int cid = r.getInt(1);
+		int mid = r.getInt(2);
+		float x1 = r.getFloat(9);
+		float y1 = r.getFloat(10);
+		float x2 = r.getFloat(11);
+		float y2 = r.getFloat(12);
+		printf("%i, %i, %g, %g, %g, %g\n", cid, mid, x1, y1, x2, y2);
+	}
+
+	printf("Opening Map.dbc...\n");
+	DBCFile map("DBFilesClient\\Map.dbc");
 
 	return 0;
 }
