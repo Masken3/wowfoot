db0b4dda786306ca0b12669cbdf4a0d83917d953
Masken3 <masken3@gmail.com>
2011-04-26 20:26:53 +0200

    Added support for paths on the form "/name=id". Test path: area=.

--- wowfoot-webrick/ww.rb
+++ wowfoot-webrick/ww.rb
@@ -5,24 +5,80 @@ include WEBrick
 require 'erb'
 require '../wowfoot-ex/output/WorldMapArea.rb'
 
-s = HTTPServer.new( :Port => 3001, :DocumentRoot => File.dirname(__FILE__) + "/htdocs" )
+S = HTTPServer.new( :Port => 3001 )#, :DocumentRoot => File.dirname(__FILE__) + "/htdocs" )
 
-class AreasServlet < HTTPServlet::AbstractServlet
+class MyPageServlet < HTTPServlet::AbstractServlet
+	def initialize(path)
+		@path = path
+	end
+	def get_instance(a)
+		return self
+	end
 	def do_GET(req, response)
-		if(req.path != '/areas')
-			raise HTTPStatus[404]
-		end
-		File.open('htdocs/areas.rhtml','r') do |f|
+		File.open("htdocs#{@path}.rhtml",'r') do |f|
 			@template = ERB.new(f.read)
 		end
-		response.body = @template.result
+		response.body = getBody(req)
 		response['Content-Type'] = "text/html"
 	end
 end
 
-s.mount("/areas", AreasServlet)
+class SinglePageServlet < MyPageServlet
+	def getBody(req)
+		if(req.path != @path)
+			raise HTTPStatus[404], "`#{req.path}' not found."
+		end
+		@template.result
+	end
+end
+
+class IdPageServlet < MyPageServlet
+	def getBody(req)
+		#p "#{@path}=([0-9]+)"
+		id = req.path.match("#{@path}=([0-9]+)")
+		#p id
+		if(!id)
+			raise HTTPStatus[404], "`#{req.path}' not found."
+		end
+		@template.result(binding())
+	end
+end
+
+class IdClassServlet < HTTPServlet::AbstractServlet
+	@@table = {}
+	def self.mount(path, servletClass)
+		@@table[path] = servletClass
+	end
+	def do_GET(req, response)
+		md = req.path.match("([a-z/]+)=([0-9]+)")
+		path, id = md[1], md[2]
+		#p path
+		#p id
+		sc = @@table[path]
+		if(!sc)
+			raise HTTPStatus[404], "`#{req.path}' not found."
+		end
+		sc.do_GET(req, response)
+	end
+end
+
+# requires the following functions to be defined by its subclasses:
+def mountSinglePage(name)
+	path = "/#{name}"
+	S.mount(path, SinglePageServlet.new(path))
+end
+
+def mountIdPage(name)
+	path = "/#{name}"
+	IdClassServlet.mount(path, IdPageServlet.new(path))
+end
+
+mountSinglePage('areas')
+mountIdPage('area')
+
+S.mount('/', IdClassServlet)
 
 trap("INT"){
-  s.shutdown
+  S.shutdown
 }
-s.start
+S.start
