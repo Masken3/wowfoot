e88268d62e3bfbbeb27c1c1ae4ff33bae9fce461
Masken3 <masken3@gmail.com>
2011-05-16 10:02:49 +0200

    Rare creature test sql.

--- wowfoot-webrick/tdb.rb
+++ wowfoot-webrick/tdb.rb
@@ -12,13 +12,19 @@ dbc = DBI::connect('dbi:Mysql:world', 'trinity', 'trinity')
 #query = 'SELECT entry, ZoneOrSort, RequiredRaces, MinLevel, title from quest_template LIMIT 0,100;'
 #results = dbc.execute(query)
 
-area = WORLD_MAP_AREA[14]	# Durotar, for starters
+area = WORLD_MAP_AREA[3483]	# Hellfire Peninsula, for starters
 map = area[:map]
 xMax = area[:a][:x]
 yMax = area[:a][:y]
 xMin = area[:b][:x]
 yMin = area[:b][:y]
 
+xDiff = xMax - xMin
+yDiff = yMax - yMin
+
+questStarters = false
+
+if questStarters
 query = "SELECT creature.guid, creature.id, position_x, position_y, position_z, creature_questrelation.quest"+
 	", creature_template.name"+
 	" FROM creature"+
@@ -28,11 +34,28 @@ query = "SELECT creature.guid, creature.id, position_x, position_y, position_z, 
 	" AND position_x BETWEEN #{xMin} AND #{xMax}"+
 	" AND position_y BETWEEN #{yMin} AND #{yMax}"+
 	""
+else
+	CREATURE_ELITE_NORMAL = 0
+	CREATURE_ELITE_ELITE = 1
+	CREATURE_ELITE_RAREELITE = 2
+	CREATURE_ELITE_WORLDBOSS = 3
+	CREATURE_ELITE_RARE = 4
+
+query = "SELECT creature.guid, creature.id, position_x, position_y, position_z"+
+	", creature_template.name"+
+	", creature_template.rank"+
+	" FROM creature"+
+	" INNER JOIN creature_template ON creature.id = creature_template.entry"+
+	" WHERE map = #{map}"+
+	" AND position_x BETWEEN #{xMin} AND #{xMax}"+
+	" AND position_y BETWEEN #{yMin} AND #{yMax}"+
+	" AND creature_template.rank IN (#{CREATURE_ELITE_RARE}, #{CREATURE_ELITE_RAREELITE})"+
+	""
+end
+
+#itemQuery = "SELECT class, subclass, name, displayid, quality, sellPrice, 
 
 puts query
-
-xDiff = xMax - xMin
-yDiff = yMax - yMin
 
 stmt = dbc.prepare(query)
 stmt.execute
@@ -44,7 +67,9 @@ stmt.fetch do |row|
 	x = (row[:position_x] - xMin) / xDiff
 	y = (row[:position_y] - yMin) / yDiff
 	z = row[:position_z]
+	if(questStarters)
 	qid = row[:quest]
+	end
 	id = row[:id]
 	name = row[:name]
 	guid = row[:guid]
@@ -52,7 +77,12 @@ stmt.fetch do |row|
 	# write some sort of overlay image.
 	width = 1024
 	height = 768
+	if(questStarters)
 	puts "<whatev src=\"dot.png\" x=#{(x*width).to_i} y=#{(y*height).to_i}> gu:#{guid} id:#{id} q:#{qid} z:#{z} name: #{name}"
+	else
+		rank = row[:rank]
+	puts "<whatev src=\"dot.png\" x=#{(x*width).to_i} y=#{(y*height).to_i}> gu:#{guid} id:#{id} r:#{rank} z:#{z} name: #{name}"
+	end
 	count += 1
 end
 puts "Got #{count} rows."
