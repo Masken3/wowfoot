45a186c116d3c76ac8708fa47e95977437ecee88
Masken3 <masken3@gmail.com>
2011-05-17 08:41:25 +0200

    Search: added creatures and limits.

--- wowfoot-webrick/search.rb
+++ wowfoot-webrick/search.rb
@@ -1,18 +1,22 @@
 
-puts "Hello search!"
-q = @id[1]
-p q
+#puts "Hello search!"
+
+query = '%'+@id[1]+'%'
 
 stm = TDB::C.prepare('select entry, ZoneOrSort, SkillOrClassMask, MinLevel, QuestLevel'+
 	', RequiredRaces, PrevQuestId, NextQuestId, title'+
-	' from quest_template where title like ?')
-stm.execute('%'+q+'%')
-@res = stm.fetch_all
+	' from quest_template where title like ? LIMIT 0,100')
+stm.execute(query)
+@quests = stm.fetch_all
 
-puts "#{@res.size} rows"
+#puts "#{@q.size} rows"
 #p res
-
 #p res[0][:entry]
 
-puts "end"
+stm = TDB::C.prepare('select entry, name, subname, minlevel, maxlevel, rank'+
+	' from creature_template where name like ? LIMIT 0,100')
+stm.execute(query)
+@creatures = stm.fetch_all
+
+#puts "end"
 STDOUT.flush
