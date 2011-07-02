0ae8171a6d5c3a5226f4387eaf2328cd1b45240e
Masken3 <masken3@gmail.com>
2011-07-02 13:45:14 +0200

    Removed "unsigned" part of database schema, since sqlite3-ruby doesn't support them.

--- wowfoot-import/create-db.sql
+++ wowfoot-import/create-db.sql
@@ -1,6 +1,6 @@
 
 DROP TABLE IF EXISTS `quest_comments`;
-CREATE TABLE `quest_comments` (`entry` int unsigned NOT NULL, `commentId` int unsigned NOT NULL);
+CREATE TABLE `quest_comments` (`entry` int NOT NULL, `commentId` int NOT NULL);
 
 DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (`id` int unsigned NOT NULL, `user` varchar(50) NOT NULL, `body` varchar(100), `rating` int unsigned NOT NULL, `date` varchar(16) NOT NULL, `indent` int unsigned NOT NULL, PRIMARY KEY (`id`));
+CREATE TABLE `comments` (`id` int NOT NULL, `user` varchar(50) NOT NULL, `body` varchar(100), `rating` int NOT NULL, `date` varchar(16) NOT NULL, `indent` int NOT NULL, PRIMARY KEY (`id`));
