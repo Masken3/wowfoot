66f524dcb0f887dec0e36aaa603cd90a828cdf80
Masken3 <masken3@gmail.com>
2011-07-06 20:55:01 +0200

    Added item, achievement, zone, faction, and itemset.

--- wowhead-downloader/curl-sql.rb
+++ wowhead-downloader/curl-sql.rb
@@ -3,6 +3,12 @@
 require 'mysql'
 require 'fileutils'
 require './config.rb'
+require '../../wowfoot-ex/output/Achievement.rb'
+require '../../wowfoot-ex/output/Faction.rb'
+require '../../wowfoot-ex/output/ItemSet.rb'
+require '../../wowfoot-ex/output/WorldMapArea.rb'
+
+@hasPrintedVersion = false
 
 def download(name, query, num_length)
 begin
@@ -13,7 +19,10 @@ begin
 	# connect to the MySQL server
 	dbh = Mysql.real_connect(SQL_ADDRESS, SQL_USER, SQL_PASS, SQL_DATABASE)
 	# get server version string and display it
-	puts "Server version: " + dbh.get_server_info
+	if(!@hasPrintedVersion)
+		puts "Server version: " + dbh.get_server_info
+		@hasPrintedVersion = true
+	end
 
 	res = dbh.query(query)
 	nFiles = 0
@@ -22,6 +31,7 @@ begin
 	
 	FileUtils.mkdir(dstDir) if(!File.exist?(dstDir))
 
+	puts query
 	while row = res.fetch_row do
 		#p row
 		q = row[0].to_s
@@ -44,7 +54,7 @@ begin
 	
 	return if(nFiles == 0)
 
-	cmd = 'curl -K curl-config.txt'
+	cmd = 'C:/path/curl -K curl-config.txt'
 	puts cmd
 	if(!system(cmd))
 		puts "Curl error: #{$?}"
@@ -60,6 +70,49 @@ ensure
 end
 end
 
+def downloadMapId(name, map, num_length)
+	file = open('curl-config.txt', 'w')
+	file.puts 'header = "accept-encoding: gzip"'
+	file.puts 'max-time = 10'
+
+	nFiles = 0
+	dstDir = "#{DST}#{name}"
+	FileUtils.mkdir(dstDir) if(!File.exist?(dstDir))
+
+	map.each do |id, hash|
+		q = id.to_s
+		while(q.length < num_length)
+			q = '0' + q
+		end
+		next if(q.length > num_length)	# avoid custom additions.
+		fn = "#{dstDir}/#{name}=#{q}.gz"
+		if(!File.exist?(fn))
+			file.puts "url = \"http://old.wowhead.com/#{name}=#{q}"
+			file.puts "output = \"#{fn}\""
+			nFiles += 1
+		end
+	end
+	puts name
+	puts "Number of rows: #{map.size}"
+	puts "Number of files to retrieve: #{nFiles}"
+
+	file.close
+
+	return if(nFiles == 0)
+
+	cmd = 'C:/path/curl -K curl-config.txt'
+	puts cmd
+	if(!system(cmd))
+		puts "Curl error: #{$?}"
+		raise hell
+	end
+end
+
+downloadMapId('achievement', ACHIEVEMENT, 4)
+downloadMapId('zone', WORLD_MAP_AREA, 4)
+downloadMapId('faction', FACTION, 4)
+downloadMapId('itemset', ITEM_SET, 4)
 download('quest', 'SELECT entry FROM quest_template', 5)
 download('npc', 'SELECT entry FROM creature_template', 5)
 download('object', 'SELECT entry FROM gameobject_template', 6)
+download('item', 'SELECT entry FROM item_template', 5)
