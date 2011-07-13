7a86c389e0308e16535a74af99f935569cf07243
Masken3 <masken3@gmail.com>
2011-07-13 17:56:14 +0200

    Item: Added drop and pickpocket tables. Started work on sources of items for disenchant table.

--- wowfoot-webrick/src/item.rb
+++ wowfoot-webrick/src/item.rb
@@ -14,16 +14,94 @@ stm.execute(@template[:disenchantID])
 #p @disenchantTo
 @disenchantTo = [] if(!@disenchantTo)	# temp hack
 
+loots = [
+	'creature',
+	'disenchant',
+	'gameobject',
+	'item',
+	'milling',
+	'pickpocketing',
+	'prospecting',
+	'reference',
+	'skinning',
+	'spell',
+]
+
+loot_entries = ''
+loot_joins = ''
+loots.each do |l|
+	loot_entries += ", #{l[0..2]}lt.entry as #{l}Loot"
+	loot_joins += " LEFT JOIN #{l}_loot_template #{l[0..2]}lt on #{l[0..2]}lt.item = it.entry"
+end
+
 stm = TDB::C.prepare('select it.entry, it.name, dlt.chanceOrQuestChance, dlt.mincountOrRef, dlt.maxcount'+
+	', clt.entry as lootID'+
+	', glt.entry as gameobjectLoot'+
+	', plt.entry as pickpocketLoot'+
 	' from item_template it'+
 	' INNER JOIN disenchant_loot_template dlt on dlt.entry = it.disenchantID'+
-	' where dlt.item = ?')
+	' LEFT JOIN creature_loot_template clt on clt.item = it.entry'+		# works if you have an index on clt.item.
+	' LEFT JOIN gameobject_loot_template glt on glt.item = it.entry'+
+	' LEFT JOIN pickpocketing_loot_template plt on plt.item = it.entry'+
+	' where dlt.item = ?'+
+	' GROUP BY it.entry')
 stm.execute(@id)
 @disenchantFrom = stm.fetch_all
+
+stm = TDB::C.prepare('select ct.entry, ct.name, clt.chanceOrQuestChance, clt.mincountOrRef, clt.maxcount'+
+	' from creature_template ct'+
+	' INNER JOIN creature_loot_template clt on clt.entry = ct.lootID'+
+	' where clt.item = ?')
+stm.execute(@id)
+@drop = stm.fetch_all
+
+stm = TDB::C.prepare('select ct.entry, ct.name, plt.chanceOrQuestChance, plt.mincountOrRef, plt.maxcount'+
+	' from creature_template ct'+
+	' INNER JOIN pickpocketing_loot_template plt on plt.entry = ct.pickpocketLoot'+
+	' where plt.item = ?')
+stm.execute(@id)
+@pickpocket = stm.fetch_all
 
 # column format: [title, array key, link array key, link page name]
 # link parts are optional.
 @TAB_TABLES = [
+{
+	:id => 'disenchantFrom',
+	:array => @disenchantFrom,
+	:title => 'Disenchanted from',
+	:columns => [
+		['Name', :name, :entry, 'item'],	# item name
+		['Chance', :chanceOrQuestChance],
+		['MinCount', :mincountOrRef],
+		['MaxCount', :maxcount],
+		['Loot', :lootID],
+		['Pickpocket', :pickpocketLoot],
+		['Contained in object', :gameobjectLoot],
+	],
+},
+{
+	:id => 'pickpocket',
+	:array => @pickpocket,
+	:title => 'Pickpocketed from',
+	:columns => [
+		['Name', :name, :entry, 'npc'],	# creature name
+#		['Location', :location],	# zone & area
+		['Chance', :chanceOrQuestChance],
+		['MinCount', :mincountOrRef],
+		['MaxCount', :maxcount],
+	],
+},
+{
+	:id => 'drop',
+	:array => @drop,
+	:title => 'Dropped by',
+	:columns => [
+		['Name', :name, :entry, 'npc'],	# creature name
+		['Chance', :chanceOrQuestChance],
+		['MinCount', :mincountOrRef],
+		['MaxCount', :maxcount],
+	],
+},
 {
 	:id => 'disenchantTo',
 	:array => @disenchantTo,
@@ -35,21 +113,9 @@ stm.execute(@id)
 		['MaxCount', :maxcount],
 	],
 },
-{
-	:id => 'disenchantFrom',
-	:array => @disenchantFrom,
-	:title => 'Disenchanted from',
-	:columns => [
-		['Name', :name, :entry, 'item'],	# item name
-		['Chance', :chanceOrQuestChance],
-		['MinCount', :mincountOrRef],
-		['MaxCount', :maxcount],
-	],
-},
 ]
 
 @TAB_TABLES2 = [
-'Disenchants into',
 'Contains',
 {
 	:id => 'reagent',
@@ -71,15 +137,6 @@ stm.execute(@id)
 		['MinLevel', :MinLevel],	# clvl or plvl
 		['Cost', :const],	# including any other reagents
 		['Result', :result],	# spell effect (damage, buff application, item creation)
-	],
-},
-{
-	:id => 'drop',
-	:array => @drop,
-	:title => 'Dropped by',
-	:columns => [
-		['Name', :name, true],	# creature name
-		['Location', :location],	# zone & area
 	],
 },
 {
@@ -119,15 +176,6 @@ stm.execute(@id)
 	:id => 'seller',
 	:array => @seller,
 	:title => 'Sold by',
-	:columns => [
-		['Name', :name, true],	# creature name
-		['Location', :location],	# zone & area
-	],
-},
-{
-	:id => 'pickpocket',
-	:array => @pickpocket,
-	:title => 'Pickpocketed from',
 	:columns => [
 		['Name', :name, true],	# creature name
 		['Location', :location],	# zone & area
