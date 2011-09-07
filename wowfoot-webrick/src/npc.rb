9031febe10b7c7102fc54b0c3e26929c02881188
Masken3 <masken3@gmail.com>
2011-09-07 11:03:17 +0200

    NPC: 404 instead of 500 if not found.

--- wowfoot-webrick/src/npc.rb
+++ wowfoot-webrick/src/npc.rb
@@ -5,6 +5,8 @@ stm = TDB::C.prepare('select entry, name, subname, minlevel, maxlevel, rank'+
 stm.execute(@id)
 @template = stm.fetch
 
+raise HTTPStatus[404], "NPC not found in database" if(!@template)
+
 stm = TDB::C.prepare('select guid, map, position_x, position_y, position_z'+
 	' from creature where id = ?')
 stm.execute(@id)
