7214202e1fd142809a4624bf1e8f334467b2fb45
Masken3 <masken3@gmail.com>
2013-10-22 23:02:08 +0200

    dbcFactionTemplate: added fields.

--- wowfoot-cpp/handlers/dbcFactionTemplate/dbcFactionTemplate.rb
+++ wowfoot-cpp/handlers/dbcFactionTemplate/dbcFactionTemplate.rb
@@ -5,4 +5,10 @@
 @id = 0
 @struct = [
 m(:int, 'nameId', 1),	# ref to Faction.dbc
+m(:int, 'flags', 2),
+m(:int, 'ourMask', 3),
+m(:int, 'friendlyMask', 4),
+m(:int, 'hostileMask', 5),
+ar(:int, 'enemyFaction', 6, 4),
+ar(:int, 'friendFaction', 10, 4),
 ]
