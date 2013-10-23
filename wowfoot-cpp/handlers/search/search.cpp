ca5c9f5dcbaea55317a54258529b902b7b617ca4
Masken3 <masken3@gmail.com>
2013-10-23 18:15:21 +0200

    search: npc subNames searchable. Spawn count shown.

--- wowfoot-cpp/handlers/search/search.cpp
+++ wowfoot-cpp/handlers/search/search.cpp
@@ -9,6 +9,7 @@
 #include "db_quest.h"
 #include "dbcItemSet.h"
 #include "dbcFaction.h"
+#include "../item/item_shared.h"
 
 #if CONFIG_WOW_VERSION > 30000
 #include "dbcAchievement.h"
@@ -23,23 +24,43 @@
 
 using namespace std;
 
-enum TableRowId {
-	NAME = ENTRY+1,
-};
-#define MAX_COUNT 100
+#define MAX_COUNT 1000
 
 static const char* cstr(const char* s) { return s; }
 static const char* cstr(const string& s) { return s.c_str(); }
 
+template<class T> class ExtraColumn {
+public:
+	typedef void (*RowFunction)(Row&, const typename T::valueType&);
+	RowFunction f;
+	const char* colName;
+	int colId;
+};
+
+static void spawnCountRowFunction(Row& r, const Npc& s) {
+	r[SPAWN_COUNT] = toString(s.spawnCount);
+	if(s.subName.length() > 0)
+		r[NAME] += " <" + s.subName + ">";
+}
+
+static void spawnCountRowFunction(Row& r, const Object& s) {
+	r[SPAWN_COUNT] = toString(s.spawnCount);
+}
+
+template<class T> class SpawnCountRow : public ExtraColumn<T> {
+private:
+public:
+	SpawnCountRow() {
+		ExtraColumn<T>::f = &spawnCountRowFunction;
+		ExtraColumn<T>::colName = "Spawn count";
+		ExtraColumn<T>::colId = SPAWN_COUNT;
+	}
+};
+
 template<class T, class StringType>
-tabTableChtml* standardSearch2(const T& map, const char* urlPart, const char* title,
-	StringType T::valueType::* namePtr)
+void standardSearchPart(tabTableChtml& t, const T& map, const char* urlPart,
+	StringType T::valueType::* namePtr, const ExtraColumn<T>* extra)
 {
-	tabTableChtml* tp = new tabTableChtml;
-	tabTableChtml& t(*tp);
-	t.id = map.name;
-	t.title = title;
-	t.columns.push_back(Column(NAME, "Name", ENTRY, map.name));
 	for(typename T::citr itr = map.begin();
 		itr != map.end() && t.array.size() < MAX_COUNT;
 		++itr)
@@ -50,16 +71,36 @@ tabTableChtml* standardSearch2(const T& map, const char* urlPart, const char* ti
 			Row r;
 			r[ENTRY] = toString(itr->first);
 			r[NAME] = s.*namePtr;
+			if(extra) {
+				extra->f(r, s);
+			}
 			t.array.push_back(r);
 		}
 	}
+}
+
+template<class T, class StringType>
+tabTableChtml* standardSearch2(const T& map, const char* urlPart, const char* title,
+	StringType T::valueType::* namePtr, const ExtraColumn<T>* extra = NULL)
+{
+	tabTableChtml* tp = new tabTableChtml;
+	tabTableChtml& t(*tp);
+	t.id = map.name;
+	t.title = title;
+	t.columns.push_back(Column(NAME, "Name", ENTRY, map.name));
+	if(extra) {
+		t.columns.push_back(Column(extra->colId, extra->colName));
+	}
+	standardSearchPart(t, map, urlPart, namePtr, extra);
 	t.count = t.array.size();
 	return tp;
 }
 
 template<class T>
-tabTableChtml* standardSearch(const T& map, const char* urlPart, const char* title) {
-	return standardSearch2(map, urlPart, title, &T::valueType::name);
+tabTableChtml* standardSearch(const T& map, const char* urlPart, const char* title,
+	const ExtraColumn<T>* extra = NULL)
+{
+	return standardSearch2(map, urlPart, title, &T::valueType::name, extra);
 }
 
 void searchChtml::getResponse2(const char* u, DllResponseData* drd, ostream& os) {
@@ -105,8 +146,17 @@ void searchChtml::getResponse2(const char* u, DllResponseData* drd, ostream& os)
 	mTabs.push_back(standardSearch(gSpells, urlPart, "Spells"));
 	mTabs.push_back(standardSearch(gItems, urlPart, "Items"));
 	mTabs.push_back(standardSearch(gItemSets, urlPart, "Item Sets"));
-	mTabs.push_back(standardSearch(gNpcs, urlPart, "NPCs"));
-	mTabs.push_back(standardSearch(gObjects, urlPart, "Objects"));
+	{
+		SpawnCountRow<Npcs> scr;
+		auto& t = *standardSearch(gNpcs, urlPart, "NPCs", &scr);
+		standardSearchPart(t, gNpcs, urlPart, &Npc::subName, &scr);
+		t.count = t.array.size();
+		mTabs.push_back(&t);
+	}
+	{
+		SpawnCountRow<Objects> scr;
+		mTabs.push_back(standardSearch(gObjects, urlPart, "Objects", &scr));
+	}
 #if CONFIG_WOW_VERSION > 30000
 	mTabs.push_back(standardSearch(gAchievements, urlPart, "Achievements"));
 	mTabs.push_back(standardSearch(gTitles, urlPart, "Titles"));
